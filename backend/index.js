48a815ce6bd88929626fa2fa943183e159b8c2a0
martinfjant <martin@falkjohansson.se>
2020-04-28 22:14:45 +0200

    Added connection between front and back!

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,37 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
+let todos = {
+  '0000000001': {
+    id: '0000000001',
+    title: 'First List',
+    todos: ['First todo of first list!'],
+  },
+  '0000000002': {
+    id: '0000000002',
+    title: 'Second List',
+    todos: ['First todo of second list!'],
+  },
+};
 
-app.use(cors())
+app.use(cors());
+app.use(bodyParser.json());
 
-const PORT = 3001
+const PORT = 3001;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/todos', (req, res) => {
+  console.log('GET');
+  res.send(todos);
+});
+app.post('/todos', (req, res) => {
+  console.log('Adding todos');
+  console.log(req.body);
+  todos = req.body;
+  console.log('Returning added todos');
+  console.log(todos);
+  res.send(todos);
+});
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
